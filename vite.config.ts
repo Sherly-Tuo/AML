7890ae8f71551c0eaa2848f91a838cbffd0c6312
Sherly Tuo <sherly-tuo@users.noreply.github.com>
2026-04-28 10:22:12 -0400

    Support both Vercel and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/AML/',
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [react(), tsconfigPaths()],
   server: {
     port: 4173,
